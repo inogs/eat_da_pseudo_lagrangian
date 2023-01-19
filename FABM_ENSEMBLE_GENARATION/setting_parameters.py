3d383091d32c5f4e41460c85b55e987812118900
Paolo Lazzari <plazzari@ogs.trieste.it>
2023-01-19 11:30:19 +0100

    tools to generate an ensemble of yaml files

---
+++ FABM_ENSEMBLE_GENARATION/setting_parameters.py
@@ -0,0 +1,17 @@
+range = 0.3
+
+parametersDICT = {
+   'B1.p_pu_ra':  [     0.6, range],
+   'Z5.p_sum':    [       2, range],
+   'Z6.p_sum':    [       5, range],
+   'P1.p_qlcPPY': [   0.026, range],
+   'P3.p_qlcPPY': [    0.02, range],
+   'P3.p_qpcPPY': [0.000786, range],
+   'P1.p_qplc':   [ 0.00043, range],
+   'P1.p_srs':    [     0.1, range],
+   'P1.p_sum':    [     2.5, range],
+   'R6.rm':       [       3, range],
+}
+
+
+
