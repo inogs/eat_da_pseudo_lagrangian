f1ab4d0f2597f46878cbe08477a1d43283cc79c2
Paolo Lazzari <plazzari@ogs.trieste.it>
2023-03-03 08:03:16 +0100

    parallel creation of F3 assimilation phase

--- F3_create_assimilation.sh
+++ F3_create_assimilation.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 #conda activate eat
+#export PYTHONPATH=$PYTHONPATH:/g100_work/OGS21_PRACE_P/COPERNICUS/bit.sea
+export PYTHONPATH=$PYTHONPATH:/g100_work/OGS_devC/V10C/RUNS_SETUP/SEAMLESS/bit.sea
 BASE_DIR=$PWD
 
 . setup.sh
@@ -12,6 +14,7 @@ N_float=${#float_list[@]}
 N_ENSEMBLE=100
 
 declare -a param_list
+
 param_list=('instances_light_parameters_EPS0r'
              'instances_Z5_parameters_p_pu'
              'instances_light_parameters_pEIR_eow'
@@ -22,6 +25,7 @@ param_list=('instances_light_parameters_EPS0r'
              'instances_P1_parameters_p_alpha_chl'
              'instances_P1_parameters_p_qplc'
              'instances_P1_parameters_p_srs') 
+
 N_param=${#param_list[@]}
 
 START=20190101
@@ -31,9 +35,13 @@ END__=20200101
 START_NUDG=20180101
 END___NUDG=20210101
 
-rm -f assimilation_F3_folder_list.txt
-for ((i=0;i<N_float; i++)); do
-    for ((p=0;p<N_param; p++)); do
+#rm -f assimilation_F3_folder_list.txt
+
+i=$1 # float index
+p=$2 # parameter index
+
+#for ((i=0;i<N_float; i++)); do
+#    for ((p=0;p<N_param; p++)); do
        cd $BASE_DIR
        echo ${float_list[${i}]}
        float=${float_list[${i}]}
@@ -47,6 +55,14 @@ for ((i=0;i<N_float; i++)); do
 # create data assimilation folder for each float
        rm -rf $WRKDIR
        cp -r $ENSDIR  $WRKDIR
+
+#extract profile from float as observation files
+       cd $BASE_DIR/SETTING_ARGO/EXTRACT_PROFILES/
+       bash launcher.sh ${float} ${START} ${END__} $WRKDIR/ToAssimilate
+
+#extract sat data along float track as observation files
+       cd $BASE_DIR/SETTING_SAT/EXTRACT_DATA/
+       bash launcher.sh ${float} $WRKDIR/ToAssimilate
    
 #create fabm files with different parameters
        cd $BASE_DIR/FABM_YAML_ASSIMILATION_GENARATION
@@ -59,7 +75,7 @@ for ((i=0;i<N_float; i++)); do
        eat-gotm-gen yaml gotm.yaml ${N_ENSEMBLE} -f fabm/yaml_file
        cp ${BASE_DIR}/runESTKF_template.py .
    
-   done
-done
+#   done
+#done
 
 #conda deactivate
